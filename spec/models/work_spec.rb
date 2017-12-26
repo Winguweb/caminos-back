c1994e10b3614527a879941c0a1d81f29cc3b319
Carlos <rockarlos@me.com>
2017-12-26 14:15:15 -0600

    add some specs

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Work, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:neighborhood) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:address) }
 end
