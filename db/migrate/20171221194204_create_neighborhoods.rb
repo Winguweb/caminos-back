aaf8a2b11d5af874a21b4f4bf7be9a088c027899
Carlos <rockarlos@me.com>
2017-12-29 13:06:56 -0600

    set ambassador with user relation to neighborhood

--- db/migrate/20171221194204_create_neighborhoods.rb
+++ db/migrate/20171221194204_create_neighborhoods.rb
@@ -3,11 +3,10 @@ class CreateNeighborhoods < ActiveRecord::Migration[5.1]
     create_table :neighborhoods, id: :uuid do |t|
       t.string :name
       t.text :description
-      t.string :ambassador
-      t.string :manager
       t.geometry :location
-
+    
       t.timestamps
     end
+
   end
 end
