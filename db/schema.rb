7dd62a5a18caf0acb31851c30eff799523d526ba
Carlos <rockarlos@me.com>
2018-01-03 17:21:23 -0600

    add organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221194205) do
+ActiveRecord::Schema.define(version: 20180103174223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20171221194205) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "topics"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "profiles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -32,15 +39,6 @@ ActiveRecord::Schema.define(version: 20171221194205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "responsibles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "neighborhood_id"
-    t.uuid "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["neighborhood_id"], name: "index_responsibles_on_neighborhood_id"
-    t.index ["user_id"], name: "index_responsibles_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -64,9 +62,12 @@ ActiveRecord::Schema.define(version: 20171221194205) do
     t.integer "roles_mask"
     t.jsonb "settings", default: {}, null: false
     t.datetime "deleted_at"
+    t.string "entity_type"
+    t.uuid "entity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["entity_type", "entity_id"], name: "index_users_on_entity_type_and_entity_id"
     t.index ["perishable_token"], name: "index_users_on_perishable_token", unique: true
     t.index ["persistence_token"], name: "index_users_on_persistence_token", unique: true
     t.index ["roles_mask"], name: "index_users_on_roles_mask"
