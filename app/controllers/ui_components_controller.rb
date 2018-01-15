c8c7ea83c5dc2c1a5819119541e676e233e0d44f
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-01-15 15:33:16 -0300

    154328312_description_text

--- app/controllers/ui_components_controller.rb
+++ app/controllers/ui_components_controller.rb
@@ -19,5 +19,6 @@ class UiComponentsController < ApplicationController
     }
     @section_footer_link_left = {:title => 'Link a la izquierda', :href => '#'}
     @section_footer_link_right = {:title => 'Link a la derecha', :href => '#'}
+    @description_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore repellendus voluptatem nobis vel quo a fugit rerum velit obcaecati officia necessitatibus, aspernatur pariatur repudiandae saepe neque non blanditiis. Omnis, reprehenderit!"
   end
 end
