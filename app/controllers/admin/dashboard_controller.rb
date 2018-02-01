08e316b7f3cf666c62747f680642c7bd2383590b
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-02-01 16:27:50 -0300

    remove tabs

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class DashboardController < ApplicationController
-  	def show
-    	@users = User.preload(:profile).all
-  	end
+    def show
+      @users = User.preload(:profile).all
+    end
   end
 end
