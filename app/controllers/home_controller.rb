02531d3fc2ec865deb12fb016cb609188d4e0a7c
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-03-08 15:58:50 -0300

    public layoout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
 
   def show
+    load_neighborhood
+  end
+
+  private
+
+  def load_neighborhood
+    @neighborhood = Neighborhood.first
   end
 
 end
