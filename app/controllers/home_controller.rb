c36313f8e3ffbadeb9344b2cb20f34c4c5f4c783
Carlos <rockarlos@me.com>
2018-03-13 12:27:26 -0600

    neighborhood selected by user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,24 @@ class HomeController < ApplicationController
   def show
     load_works
     load_neighborhood
+    @neighborhoods = Neighborhood.all
+    
   end
 
   private
 
   def load_neighborhood
     @neighborhood = Neighborhood.first
+    if params[:neighborhood].present? && Neighborhood.exists?(id: params[:neighborhood])
+        @neighborhood = Neighborhood.find(params[:neighborhood])
+    end
   end
 
   def load_works
     @works = Work.all
+    if params[:neighborhood].present? && Neighborhood.exists?(id: params[:neighborhood])
+        @works = Neighborhood.find(params[:neighborhood]).works
+    end
   end
 
 end
