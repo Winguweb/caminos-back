82b138aa7cd296ab23084dee2cea7f1a046caeb9
Carlos <rockarlos@me.com>
2018-03-21 17:44:01 -0600

    fiex edit view

--- app/controllers/admin/works_controller.rb
+++ app/controllers/admin/works_controller.rb
@@ -50,9 +50,9 @@ module Admin
       service = UpdateWork.call(@work, work_params)
 
       if service.success?
-        redirect_to neighborhood_work_path(@work)
+        redirect_to admin_neighborhood_work_path
       else
-        redirect_to edit_neighborhood_work_path(@work)
+        redirect_to edit_admin_neighborhood_work_path(@work)
       end
     end
 
