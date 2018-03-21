9c8254302f1c18c4b3fc7952109a0774ce9286b8
Carlos <rockarlos@me.com>
2018-03-21 15:44:07 -0600

    add categories & status

--- app/models/work.rb
+++ app/models/work.rb
@@ -17,6 +17,17 @@ class Work < ApplicationRecord
 
   validate :valid_dates
 
+  CATEGORIES =['Servicios Públicos','Equipamiento Estatal','Vivienda','Espacios Públicos'].freeze
+  STATUS =['En proceso','Finalizadas', 'Pendientes','Vencidas','Proyectadas'].freeze
+
+  def self.status
+    STATUS
+  end
+
+  def self.categories
+    CATEGORIES
+  end
+
   def valid_dates
     if start_date && estimated_end_date && (estimated_end_date < start_date)
       errors.add(:invalid_dates, "invalid_dates please check now")
