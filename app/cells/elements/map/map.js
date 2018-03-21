29463d78b5f65b85c161a557d535a73cd409a46c
Carlos <rockarlos@me.com>
2018-03-21 14:13:52 -0600

    fix contros for map

--- app/cells/elements/map/map.js
+++ app/cells/elements/map/map.js
@@ -11,34 +11,58 @@ CDLV.Components['map'] = Backbone.View.extend({
     var markers = options.markers
 
     if (options.set_polygon) {
-      var drawnItems = new L.featureGroup();
-       var drawControl = new L.Control.Draw({
-           edit: {
-               featureGroup: drawnItems
-           }
-       });
-       map.addControl(drawControl);
+      var controls = {
+        position: 'topright',
+        draw: {
+          polyline: {
+            shapeOptions: {
+              color: '#f357a1',
+              weight: 10
+            }
+          },
+          polygon: {
+            allowIntersection: false, // Restricts shapes to simple polygons
+            drawError: {
+              color: '#e1e100', // Color the shape will turn when intersects
+              message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+            },
+            shapeOptions: {
+              color: '#bada55'
+            }
+          },
+          circle: false, // Turns off this drawing tool
+          rectangle: {
+            shapeOptions: {
+              clickable: false
+            }
+          },
+          marker: false
+        },
+      };
+    
+      var drawControl = new L.Control.Draw(controls);
+      map.addControl(drawControl);
 
       map.on('draw:created', function (e) {
-          map.addLayer(e.layer);
-          var latLngs = e.layer.getLatLngs()
-          var new_polygon = new L.Polygon(latLngs)
-          // var coordinates = evt.latlng
-          // var point = new L.Point(coordinates.lat, coordinates.lng)
-          // set_marker(map, coordinates)
-          var new_polygon_geojson = new_polygon.toGeoJSON()
-          var new_polygon_type = new_polygon_geojson.geometry.type.toUpperCase()
-          var new_polygon_points = []
-          var points = new_polygon_geojson.geometry.coordinates[0]
-          for (var i in points) {
-            var point = points[i]
-            new_polygon_points.push(point[0] + " " + point[1])
-          }
+        map.addLayer(e.layer);
+        var latLngs = e.layer.getLatLngs()
+        var new_polygon = new L.Polygon(latLngs)
+        // var coordinates = evt.latlng
+        // var point = new L.Point(coordinates.lat, coordinates.lng)
+        // set_marker(map, coordinates)
+        var new_polygon_geojson = new_polygon.toGeoJSON()
+        var new_polygon_type = new_polygon_geojson.geometry.type.toUpperCase()
+        var new_polygon_points = []
+        var points = new_polygon_geojson.geometry.coordinates[0]
+        for (var i in points) {
+          var point = points[i]
+          new_polygon_points.push(point[0] + " " + point[1])
+        }
 
-          var new_polygon_points_string = new_polygon_points.join(', ')
-          new_polygon_string = new_polygon_type + " ((" + new_polygon_points_string +  "))"
-          input_geometry.val(new_polygon_string)
-          input_geo_geometry.val(new_polygon_string)
+        var new_polygon_points_string = new_polygon_points.join(', ')
+        new_polygon_string = new_polygon_type + " ((" + new_polygon_points_string +  "))"
+        input_geometry.val(new_polygon_string)
+        input_geo_geometry.val(new_polygon_string)
       });
     }
 
@@ -49,8 +73,8 @@ CDLV.Components['map'] = Backbone.View.extend({
       var new_polygon_points = []
       
       for (var i in copypoint) {
-          var point = copypoint[i];
-          new_polygon_points.push(point[1] + " " + point[0]);
+        var point = copypoint[i];
+        new_polygon_points.push(point[1] + " " + point[0]);
 
       }
       var new_polygon_points_string =  new_polygon_points.join(', ');
