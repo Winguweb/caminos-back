5493b65ea68d9f4121f1076c88910ca496682504
Carlos <rockarlos@me.com>
2018-03-22 16:02:17 -0600

    update carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,11 @@
+env = ENV['RAILS_ENV'] || Rails.env
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY_CAMINOS'],
-      aws_secret_access_key: ENV['S3_SECRET_CAMINOS'],
-      region:                ENV['S3_REGION_CAMINOS'],
+      aws_access_key_id:     APP_CONFIG[env]['s3_key'].freeze,
+      aws_secret_access_key: APP_CONFIG[env]['s3_secret'].freeze,
+      region:                APP_CONFIG[env]['s3_region'].freeze,
   }
-  config.fog_directory  = ENV['S3_BUCKET_CAMINOS']
+  config.fog_directory  = APP_CONFIG[env]['s3_bucket'].freeze
 end
