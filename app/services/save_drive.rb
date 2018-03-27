74bb1a921ee36afdbe8f6d14f9c516defb0ced24
Carlos <rockarlos@me.com>
2018-03-27 11:01:25 -0600

    update Gdrive config

--- app/services/save_drive.rb
+++ app/services/save_drive.rb
@@ -29,9 +29,10 @@ class SaveDrive
   def authorize
     oob_uir = 'urn:ietf:wg:oauth:2.0:oob'
     scope = Google::Apis::DriveV2::AUTH_DRIVE
-    FileUtils.mkdir_p(File.dirname(File.join(Dir.home, '.credentials',"drive-ruby-quickstart.yaml")))
+    FileUtils.mkdir_p(File.dirname(File.join(Rails.root,"config" ,"Gdrive.yaml")))
+
     client_id = Google::Auth::ClientId.new(GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET)
-    token_store = Google::Auth::Stores::FileTokenStore.new(file: File.join(Dir.home, '.credentials',"drive-ruby-quickstart.yaml"))
+    token_store = Google::Auth::Stores::FileTokenStore.new(file: File.join(Rails.root,"config","Gdrive.yaml"))
     authorizer = Google::Auth::UserAuthorizer.new(
       client_id, scope, token_store)
     user_id = 'default'
