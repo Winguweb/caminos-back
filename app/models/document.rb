3c0e9eb9618577d8bc6a66528bb7188503ed3439
Carlos <rockarlos@me.com>
2018-03-27 07:25:23 -0600

    google api client impelentation for neighborhoods documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,11 +1,6 @@
 class Document < ApplicationRecord
   belongs_to :holder, polymorphic: true
 
-  validates_presence_of :name,
-    :description,
-    :attachment_type,
-    :attachment_source,
-    :filetype,
-    :holder_type,
-    :holder_id
+  validates_presence_of :name,:description,:attachment_source
+
 end
--- app/services/create_neighborhood.rb
+++ app/services/create_neighborhood.rb
@@ -12,11 +12,41 @@ class CreateNeighborhood
   private
 
   def create_neighborhood
-    neighborhood = Neighborhood.new(@allowed_params)
-    neighborhood.agreement = Agreement.new
-    return neighborhood if neighborhood.save
+    @neighborhood = Neighborhood.new(neighborhood_params)
+    @neighborhood.agreement = Agreement.new
 
-    errors.add_multiple_errors(neighborhood.errors.messages) && nil
+    if !document_param.nil? 
+      document_param.each do |document|
+        service_document = SaveDrive.call(document[:link])
+        if (service_document != nil)
+          document =  @neighborhood.documents.new(name:document[:name], description:document[:description], attachment_source:"https://www.googleapis.com/drive/v2/files/#{service_document.result.id}")
+          document.holder = @neighborhood
+        end
+      end
+      
+      return @neighborhood if @neighborhood.save
+      errors.add_multiple_errors(@neighborhood.errors.messages) && nil
+      
+    else
+      return @neighborhood if @neighborhood.save
+      errors.add_multiple_errors(@neighborhood.errors.messages) && nil
+    end
+
+
   end
 
+  def neighborhood_params
+    {
+      name: @allowed_params[:name],
+      description: @allowed_params[:description],
+      lookup_coordinates: @allowed_params[:lookup_coordinates],
+      lookup_address: @allowed_params[:lookup_address],
+      geo_polygon: @allowed_params[:geo_polygon],
+      polygon: @allowed_params[:polygon],
+    }
+  end
+
+  def document_param
+    @allowed_params[:documents]
+  end
 end
