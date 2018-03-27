085536f8a07d7fd5e8bb89ca926f5cd7f45f8cef
Carlos <rockarlos@me.com>
2018-03-27 09:58:31 -0600

    delete coments on initializer google

--- config/initializers/google_drive.rb
+++ config/initializers/google_drive.rb
@@ -3,37 +3,3 @@
   GOOGLE_CLIENT_ID    = APP_CONFIG[env]['google_client_id'].freeze
 
   GOOGLE_CLIENT_SECRET = APP_CONFIG[env]['google_client_secret'].freeze
-# class GOOGLE
-#   env = ENV['RAILS_ENV'] || Rails.env
-
-#   GOOGLE_CLIENT_ID    = APP_CONFIG[env]['google_client_id'].freeze
-
-#   GOOGLE_CLIENT_SECRET = APP_CONFIG[env]['google_client_secret'].freeze
-
-  # def initialize(n)    
-  #   oob_uir = 'urn:ietf:wg:oauth:2.0:oob'
-  #   scope = Google::Apis::DriveV2::AUTH_DRIVE
-
-  #   FileUtils.mkdir_p(File.dirname(File.join(Dir.home, '.credentials',"drive-ruby-quickstart.yaml")))
-
-  #   client_id = Google::Auth::ClientId.new(GOOGLE_CLIENT_ID,GOOGLE_CLIENT_SECRET)
-  #   token_store = Google::Auth::Stores::FileTokenStore.new(file: File.join(Dir.home, '.credentials',"drive-ruby-quickstart.yaml"))
-  #   authorizer = Google::Auth::UserAuthorizer.new(
-  #     client_id, scope, token_store)
-  #   user_id = 'default'
-  #   credentials = authorizer.get_credentials(user_id)
-  #   if credentials.nil?
-  #     url = authorizer.get_authorization_url(
-  #       base_url: oob_uir)
-  #     puts "Open the following URL in the browser and enter the " +
-  #          "resulting code after authorization"
-  #     puts url
-  #     code = gets
-  #     credentials = authorizer.get_and_store_credentials_from_code(
-  #       user_id: user_id, code: code, base_url: oob_uir)
-  #   end
-  #   credentials   
-  # end
-# end
-
-
