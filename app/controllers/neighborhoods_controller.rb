2d7c1b8117725c831bd6c891dd6821859a9d6327
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-11 16:43:49 -0300

    documents and meetings added for no urbanizated neighborhoods at public layout

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -2,6 +2,7 @@ class NeighborhoodsController < ApplicationController
 
   def show
     load_neighborhood
+    load_meetings
   end
 
   def about
@@ -11,13 +12,16 @@ class NeighborhoodsController < ApplicationController
   def agreement
     load_neighborhood
     @data = eval(@neighborhood.agreement.data) if !@neighborhood.agreement.data.nil?
-
   end
 
   private
 
   def load_neighborhood
     @neighborhood = Neighborhood.find(params[:id])
+  end
+
+  def load_meetings
+    @meetings = @neighborhood.meetings.order(date: :desc).group_by { |x| x.date.year }
   end
 
 
