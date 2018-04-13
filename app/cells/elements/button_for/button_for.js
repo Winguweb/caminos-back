9b0b595fb6673145ee350e86f81e8b96105d837b
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-13 15:46:17 -0300

    remove unused events

--- app/cells/elements/button_for/button_for.js
+++ app/cells/elements/button_for/button_for.js
@@ -10,13 +10,6 @@ CDLV.Components['button_for'] = Backbone.View.extend({
     )
 
     this.element = $('#' + options.for.id).siblings('.jFiler-input')
-
-    CDLV.pubSub.on({
-      'filer:upload:before': this.beforeUpload,
-      'filer:upload:error': this.uploadError,
-      'filer:upload:success': this.uploadSuccess,
-      'filer:remove': this.removeFile
-    })
   },
   labelClicked: function(evt) {
     evt.preventDefault()
