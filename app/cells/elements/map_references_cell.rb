46f8bdeee2844c38ab7429ac5215dc9089744d90
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-17 11:02:05 -0300

    remove unused features from map_references

--- app/cells/elements/map_references_cell.rb
+++ app/cells/elements/map_references_cell.rb
@@ -3,55 +3,16 @@ class Elements::MapReferencesCell < Cell::ViewModel
   private
 
   def base
-    if model.kind_of?(ActiveRecord::Relation)
-      reference_number = 0
-      model.map do |neighborhood|
-        reference_number += 1
-        {
-            coordinates: neighborhood[:geometry].coordinates.first.map(&:reverse),
-            className: 'base-geometry',
-            reference: reference_number
-        }
-
-      end.to_json
-    elsif model.kind_of?(Neighborhood)
+    return [] if model.blank?
+    reference_number = 0
+    model.map do |neighborhood|
+      reference_number += 1
       {
-          coordinates: model[:geometry].coordinates.first.map(&:reverse),
+          coordinates: neighborhood[:geometry].coordinates.first.map(&:reverse),
           className: 'base-geometry',
-      }.to_json
-    else
-      return []
-    end
-  end
-
-  def features
-    return [] if options[:features].blank?
-    options[:features].map do |feature|
-      case feature[:geometry].geometry_type
-      when RGeo::Feature::Point
-        {
-          coordinates: [feature[:geometry].coordinates],
-          icon: image_path(feature.category_icon),
-          type: 'marker',
-        }
-      when RGeo::Feature::Polygon
-        {
-          coordinates: feature[:geometry].coordinates.first.map(&:reverse),
-          className: feature.category,
-          type: 'polygon',
-        }
-      when RGeo::Feature::LineString
-        {
-          coordinates: feature[:geometry].coordinates.map(&:reverse),
-          className: feature.category,
-          type: 'polyline',
-        }
-      end
+          reference: reference_number
+      }
     end.to_json
-  end
-
-  def as_references
-    return options[:as_references]
   end
 
   def map_defaults
