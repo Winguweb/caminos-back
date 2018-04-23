2f08c8e7a8ab017a802166a43f9fa605f6abe8ce
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-23 18:24:12 -0300

    mobile_views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,9 @@
 class HomeController < ApplicationController
+  before_action :check_for_mobile, :only => [:show]
 
   def show
     @neighborhoods = Neighborhood.where(urbanization: true).order('LOWER(name)')
     @unurbanized= Neighborhood.where(urbanization: false).order('LOWER(name)')
-  end
-
-  def mobile
-    @neighborhoods = Neighborhood.where(urbanization: true).order('LOWER(name)')
-    @unurbanized= Neighborhood.where(urbanization: false).order('LOWER(name)')
-    render layout: 'public_mobile'
   end
 
   private
