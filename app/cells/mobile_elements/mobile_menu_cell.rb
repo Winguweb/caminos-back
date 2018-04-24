7887281731e7879a1e0348e3241c46dfc9ecb7e0
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-24 15:27:26 -0300

    mobile sub menu

--- app/cells/mobile_elements/mobile_menu_cell.rb
+++ app/cells/mobile_elements/mobile_menu_cell.rb
@@ -1,3 +1,12 @@
 class MobileElements::MobileMenuCell < Cell::ViewModel
+  include ::Cell::Translation
 
+  def neighborhood
+    return [] if model.blank?
+    model
+  end
+
+  def links
+    options[:links]
+  end
 end
