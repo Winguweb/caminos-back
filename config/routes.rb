b3983aafdaa2e5bea6f463a4e1ddd8eda8df3b1c
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-04-24 14:36:54 -0300

    home mobile styles, neighborhoods mobile markup and styles

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
         get :agreement
         get :about
         resources :works, only: [:show]
-        get '/(:filters)', action: :filtered, controller: :neighborhoods, as: :filtered_work
+        get '/:filters', action: :show, controller: :neighborhoods, as: :filtered_work
       end
     end
     get '/components', action: :index, controller: :components
