30c4316485dd722a4418134b83a6e1814a8d4dbb
Carlos <rockarlos@me.com>
2018-05-28 14:38:28 -0500

    pass params

--- app/controllers/admin/works_controller.rb
+++ app/controllers/admin/works_controller.rb
@@ -13,6 +13,7 @@ module Admin
     end
 
     def new
+
       ensure_neighborhood; return if performed?
 
       @categories = Work.categories
@@ -28,8 +29,11 @@ module Admin
       if service.success?
         redirect_to admin_neighborhood_works_path
       else
-        flash[:error] =  load_erros(service.errors)
-        redirect_to new_admin_neighborhood_work_path(current_neighborhood)
+        flash[:error] =  load_errors(service.errors)
+        @categories = Work.categories
+        @status = Work.status
+        @work = current_neighborhood.works.new(work_params)
+        render action: :new
       end
     end
 
@@ -56,8 +60,11 @@ module Admin
       if service.success?
         redirect_to admin_neighborhood_work_path
       else
-        flash[:error] =  load_erros(service.errors)
-        redirect_to edit_admin_neighborhood_work_path(@work)
+        flash[:error] =  load_errors(service.errors)
+        @categories = Work.categories
+        @status = Work.status
+        @work = current_neighborhood.works.new(work_params)
+        render action: :new
       end
     end
 
@@ -75,7 +82,7 @@ module Admin
 
     private
 
-    def load_erros(errors)
+    def load_errors(errors)
       messages  = []
       errors.each do |error|
         messages << t('admin.works.errors', field: t("works.#{error}"))
