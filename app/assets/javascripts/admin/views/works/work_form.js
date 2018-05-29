aea244ea38014bce29e5bc1e1d778b1e59b8941d
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-05-29 13:45:12 -0300

    parent categories added to new and edit form and works show

---
+++ app/assets/javascripts/admin/views/works/work_form.js
@@ -0,0 +1,44 @@
+CDLV.Components['work_form'] = Backbone.View.extend({
+  initialize: function(options) {
+    _.bindAll(
+      this,
+      'categoryChanged'
+    )
+
+    this.categoryByParents = {
+      public_services: {keys: ["water", "trash", "public", "energy", "sewer"], title: "Servicios Públicos: "},
+      state_equipment: {keys: ["health"], title: "Equipamiento Estatal: "},
+      public_spaces: {keys: ["infrastructure"], title: "Espacios Públicos: "},
+      home: {keys: ["home"], title: "Vivienda: "},
+      empty: {keys: [""], title: "Seleccione una categoría"},
+    }
+
+    this.$select = this.$el.find('#work_category_list')
+    this.$options = this.$el.find('#work_category_list option')
+    this.$label = this.$el.find('.category-parent')
+
+    this.$select.on('change', this.categoryChanged)
+
+    for (var index = 0; index < this.$options.length; index++) {
+      var parentIndex = this.getParentFromCategory(this.$options[index].value)
+      this.$options[index].value = [this.$options[index].value, parentIndex].join(',')
+    }
+
+    this.changeParentValue(this.$select.val())
+  },
+  categoryChanged: function(el) {
+    this.changeParentValue(el.target.value)
+  },
+  changeParentValue: function(value) {
+    var parentIndex = this.getParentFromCategory(value.split(',')[0])
+    this.$label.text(this.categoryByParents[parentIndex].title)
+  },
+  getParentFromCategory: function(category) {
+    for (var index in this.categoryByParents) {
+      if (this.categoryByParents[index].keys.indexOf(category) > -1) {
+        return index
+      }
+    }
+  }
+
+})
