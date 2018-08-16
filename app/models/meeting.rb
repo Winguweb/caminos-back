41fa0de9c7332b56ed1a5193bae283ae1edd82a6
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-08-16 15:24:21 -0300

    meeting model test

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,8 +4,7 @@ class Meeting < ApplicationRecord
   belongs_to :neighborhood
   has_and_belongs_to_many :works
 
-  validates_presence_of :date,
-    :objectives
+  validates_presence_of :date, :objectives, :lookup_address
 
   default_scope { order(date: :asc) }
 
