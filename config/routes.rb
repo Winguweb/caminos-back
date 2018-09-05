688c007ab788add083f128da33a0ae220c4fe445
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-09-05 17:01:06 -0300

    landing - some pending stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope protocol: SECURE_PROTOCOL do
 
   # ╭─ Public Accesible URL's / Path's
-    root to: 'home#show'
+    root to: 'home#index'
 
     get '/mobile', action: :mobile, controller: :home
 
