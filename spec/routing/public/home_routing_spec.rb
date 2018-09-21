4cca75546bdd40db3f2e5d20695d63fc2363104f
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-09-21 12:53:03 -0300

    routes testing fixes

--- spec/routing/public/home_routing_spec.rb
+++ spec/routing/public/home_routing_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe HomeController, type: :routing do
   let(:routes_params){ { protocol: 'https' } }
 
   describe 'Home routable' do
-    it { expect(get: '/').to route_to( routes_params.merge(controller: 'home', action: 'show') ) }
+    it { expect(get: '/').to route_to( routes_params.merge(controller: 'home', action: 'index') ) }
   end
 end
--- spec/routing/public/neighborhood_routing_spec.rb
+++ spec/routing/public/neighborhood_routing_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe NeighborhoodsController, type: :routing do
-  let(:routes_params){ { protocol: 'https', id: '1' } }
+  let(:routes_params){ { protocol: 'https' } }
 
   describe 'Neighborhood NOT routable' do
-    it { expect(get:    '/neighborhoods').not_to be_routable }
     it { expect(post:   '/neighborhoods').not_to be_routable }
     it { expect(get:    '/neighborhoods/1/edit').not_to be_routable }
     it { expect(patch:  '/neighborhoods/1').not_to be_routable }
@@ -13,7 +12,8 @@ RSpec.describe NeighborhoodsController, type: :routing do
   end
 
   describe 'Neighborhood resources routable' do
-    it { expect(get: '/neighborhoods/1').to route_to( routes_params.merge(controller: 'neighborhoods', action: 'show') ) }
-    it { expect(get: '/neighborhoods/1/about').to route_to( routes_params.merge(controller: 'neighborhoods', action: 'about') ) }
+    it { expect(get: '/neighborhoods').to route_to( routes_params.merge(controller: 'neighborhoods', action: 'index') ) }
+    it { expect(get: '/neighborhoods/1').to route_to( routes_params.merge(controller: 'neighborhoods', action: 'show', id: '1') ) }
+    it { expect(get: '/neighborhoods/1/about').to route_to( routes_params.merge(controller: 'neighborhoods', action: 'about', id: '1') ) }
   end
 end
