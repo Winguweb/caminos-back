b2b7f304a2e4a03e7337cf909a89814fcf4d4c12
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-09-29 19:12:47 -0300

    map_references refactor with geoJSON

--- app/cells/elements/map_references/map_references.js
+++ app/cells/elements/map_references/map_references.js
@@ -4,13 +4,10 @@ CDLV.Components['map_references'] = Backbone.View.extend({
       this,
       'centerMap',
       'createMap',
-      'getCenter',
-      'getBounds',
       'loadDefaults',
       'setAccessToken',
       'setMapContainer',
       'showBaseGeometry',
-      'showReference'
     )
 
     this.setAccessToken(options.token)
@@ -26,17 +23,9 @@ CDLV.Components['map_references'] = Backbone.View.extend({
     this.showBaseGeometry()
 
     this.centerMap(this.base)
-
-    this.zoomMap(this.base)
   },
   centerMap: function(polygons) {
-    var polygonsCenters = polygons.map(function(polygon) {
-      var center = this.getCenter(polygon)
-      return center
-    }.bind(this))
-
-    var center = this.getCenter({coordinates: polygonsCenters}) || this.center
-    this.map.setView([center.x, center.y], this.zoom)
+    this.map.fitBounds(this.baseGeometryFeature.getBounds())
   },
   configureMapForMobile: function() {
     if ($('html').hasClass('touchevents')) {this.map.dragging.disable()}
@@ -45,18 +34,6 @@ CDLV.Components['map_references'] = Backbone.View.extend({
     this.map = L.mapbox.map(this.mapContainer[0], this.style)
     this.baseGeometryFeature = new L.FeatureGroup()
     this.map.addLayer(this.baseGeometryFeature)
-  },
-  createPoints: function(points) {
-    return points.coordinates.map(function(point) {
-      return new L.Point(point[0], point[1])
-    })
-  },
-  getBounds: function(polygon) {
-    return new L.Bounds(polygon.coordinates)
-  },
-  getCenter: function(polygon) {
-    var bounds = this.getBounds(polygon)
-    return bounds.getCenter()
   },
   loadDefaults: function(options) {
     this.center = options.defaults.center
@@ -71,27 +48,6 @@ CDLV.Components['map_references'] = Backbone.View.extend({
     this.mapContainer = this.$el.find(selector)
   },
   showBaseGeometry: function() {
-    this.base.forEach(function(polygon) {
-      this.showReference (polygon)
-    }.bind(this))
-  },
-  showReference: function(polygon) {
-    polygon.coordinates = polygon.coordinates.length == 1 ? polygon.coordinates[0] : polygon.coordinates
-    var center = this.getCenter(polygon)
-    var coordinates = new L.latLng(center.x, center.y)
-
-    new L.Marker(coordinates, {
-      icon: new L.divIcon({
-        html: '<div><p class="marker-name ' + polygon.className + '">' + polygon.name + '</p><p class="reference-marker ' + polygon.className + '"><span>' + polygon.reference + '</span></p></div>'
-      })
-    }).addTo(this.baseGeometryFeature)
-  },
-  zoomMap: function(polygons) {
-    var polygonsCenters = polygons.map(function(polygon) {
-      var center = this.getCenter(polygon)
-      return center
-    }.bind(this))
-    var bounds = this.getBounds({coordinates: polygonsCenters})
-    this.map.fitBounds([[bounds.min.x, bounds.min.y], [bounds.max.x, bounds.max.y]], {animate: false, padding: [50,50]})
+    L.geoJSON(this.base).addTo(this.baseGeometryFeature)
   },
 })
