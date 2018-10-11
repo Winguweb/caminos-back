237446fedf4066cb58b1d26d852b151b6dcebd39
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2018-10-11 19:05:44 -0300

    agreements weight and updated q & a

--- app/services/create_agreement.rb
+++ app/services/create_agreement.rb
@@ -13,24 +13,30 @@ class CreateAgreement
   private
 
   def create_agreement
-
     @agreement = @neighborhood.agreement || Agreement.new
     @agreement.assign_attributes(data: agreement_params)
     @agreement.neighborhood = @neighborhood
 
     return @agreement if @agreement.save
     errors.add_multiple_errors(@agreement.errors.messages) && nil
-
   end
 
   def agreement_params
+    indicators = Agreement.indicators
+
     @data = {}
     @params.keys.each do |indicator|
+      score = 0
       @answers = {}
       @params[indicator][:questions].keys.each do |question|
         @answers[question.parameterize.underscore.to_sym] = @params[indicator][:questions][question.parameterize.underscore.to_sym]
       end
-      @data[indicator.parameterize.underscore.to_sym] = { answers:  @answers, score:  @params[indicator][:score]}
+
+      @answers.each do |question, answer|
+        score += indicators[indicator][:questions][question][:answers].select{|test_answer| test_answer[:text] == answer}.first[:weight].round(0)
+      end
+
+      @data[indicator.parameterize.underscore.to_sym] = { answers:  @answers, score: score}
     end
     return @data.to_json
   end
--- app/services/update_agreement.rb
+++ app/services/update_agreement.rb
@@ -20,13 +20,21 @@ class UpdateAgreement
   end
 
   def agreement_params
+    indicators = Agreement.indicators
+
     @data = {}
     @params.keys.each do |indicator|
+      score = 0
       @answers = {}
       @params[indicator][:questions].keys.each do |question|
         @answers[question.parameterize.underscore.to_sym] = @params[indicator][:questions][question.parameterize.underscore.to_sym]
       end
-      @data[indicator.parameterize.underscore.to_sym] = { answers:  @answers, score:  @params[indicator][:score]}
+
+      @answers.each do |question, answer|
+        score += indicators[indicator][:questions][question][:answers].select{|test_answer| test_answer[:text] == answer}.first[:weight].round(0)
+      end
+
+      @data[indicator.parameterize.underscore.to_sym] = { answers:  @answers, score: score}
     end
     return @data.to_json
   end
