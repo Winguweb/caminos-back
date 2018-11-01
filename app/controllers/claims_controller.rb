1eb8b7f0d65b8853ad82e8109164c5ccdb745db8
rockarloz <rockarlos@me.com>
2018-11-01 11:09:06 -0600

    add claim controller

---
+++ app/controllers/claims_controller.rb
@@ -0,0 +1,41 @@
+class ClaimsController < ApplicationController
+  include CurrentAndEnsureDependencyLoader
+
+   helper_method :current_neighborhood
+
+   def create
+    ensure_neighborhood; return if performed?
+     service = CreateClaim.call(current_neighborhood, asset_params)
+     # service = CreateClaim.call(current_neighborhood, asset_params,some_work)
+     if service.success?
+      redirect_to admin_neighborhood_claims_path
+    else
+      flash.now[:error] =  load_errors(service.errors)
+      @categories = Claim.categories
+      @claim = current_neighborhood.claims.new(asset_params)
+      render action: :new
+    end
+  end
+  
+  def show
+    load_claim
+    @neighborhood = @claim.neighborhood
+  end
+  
+  private
+  
+  def load_claim
+    @claim = Claim.friendly.find(params[:id])
+  end
+  
+  def claim_params
+    params.require(:claim).permit(
+    :category_list,
+    :description,
+    :geo_geometry,
+    :geometry,
+    :lookup_address,
+    :name,
+    )
+  end
+end
