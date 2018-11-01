5a1843469e487c04d6eb00fd663ad7fd5af0cde1
rockarloz <rockarlos@me.com>
2018-11-01 11:08:57 -0600

    add create claims services

---
+++ app/services/create_claim.rb
@@ -0,0 +1,35 @@
+class CreateClaim
+  prepend Service::Base
+
+  def initialize(neighborhood, allowed_params, work=nil)
+    @neighborhood = neighborhood
+    @allowed_params = allowed_params
+    @work = work
+  end
+
+  def call
+    create_claim
+  end
+
+  private
+
+  def create_claim
+    @claim = Claim.new(claim_params)
+    @claim.neighborhood = @neighborhood
+    @claim.work = @work if @work != nil 
+    return @claim if @claim.save
+
+    errors.add_multiple_errors(@claim.errors.messages) && nil
+  end
+
+  def claim_params
+    {
+      category_list: @allowed_params[:category_list],
+      description: @allowed_params[:description],
+      geo_geometry: @allowed_params[:geo_geometry],
+      geometry: @allowed_params[:geometry],
+      lookup_address: @allowed_params[:lookup_address],
+      name: @allowed_params[:name],
+    }
+  end
+end
